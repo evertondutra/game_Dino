6b142d169ed9cbdcc6d898445bf00928401472d6
Everton Dutra <evertondutra02@gmail.com>
2021-02-14 03:40:36 -0300

    Finalizando a iteração com o dinossauro

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const dino = document.querySelector('.dino');
+let isjumping = false;
 
 function handleKeyUp(event) {
     if( event.keyCode === 32) {
-        jump();
+        if (!isjumping){
+            jump();
+        }
     }
 }
 
 function jump() {
     let position = 0;
+isjumping = true;
 
     let upInterval = setInterval (()=> {
         if (position >= 150) {
@@ -16,6 +20,7 @@ function jump() {
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
+                    isjumping = false;
                 } else {
                 position -= 20;
                 dino.style.bottom = position + 'px';
